577196869f665b1fff453d94cf32e62767a171b4
JoxueTe <josuetejeda02@gmail.com>
2024-09-06 14:48:58 -0500

    feat: add login/reset/recover/password workflows

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,12 @@
 from django.contrib.auth import views as auth_views
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 from . import views
 
 urlpatterns = [
+    path('', views.dashboard, name='dashboard'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.dashboard, name='dashboard'),
+    path('', include('django.contrib.auth.urls')),
 ]
