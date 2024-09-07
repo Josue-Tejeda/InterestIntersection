1bf61f3a8b1edb64520cd7ac751f19f9a4e1c100
JoxueTe <josuetejeda02@gmail.com>
2024-09-07 11:24:15 -0500

    feat: add user account details

--- account/models.py
+++ account/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+    )
+    bio = models.TextField(blank=True)
+    location = models.CharField(max_length=30, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    avatar = models.ImageField(
+        upload_to='users/%Y/%m/d',
+        blank=True
+    )
+    
+    def __str__(self):
+        return f'Profile for user {self.user.username}'
--- account/urls.py
+++ account/urls.py
@@ -5,8 +5,10 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
-    path('', views.dashboard, name='dashboard'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', include('django.contrib.auth.urls')),
+    path('', views.dashboard, name='dashboard'),
+    path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),  
 ]
